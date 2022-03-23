4a5629ab89bd7d51129a6add08de71410077dada
Nolan Bjarne <derjuniorschiemann@gmail.com>
2022-03-23 20:26:57 +0100

    Update gulpfile.mjs

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -7,13 +7,14 @@ Variables: ------------------------------------------------------
 */
 
 import gulp from 'gulp';
-import renameFiles from 'gulp-rename';
+import rename from 'gulp-rename';
 import dartSass from 'sass';
 import gulpSass from 'gulp-sass';
 import cleanCSS from 'gulp-clean-css';
 import sourcemaps from 'gulp-sourcemaps';
 import autoprefixer from 'gulp-autoprefixer';
 import plumber from 'gulp-plumber';
+import del from 'del';
 
 const sass = gulpSass(dartSass);
 
@@ -28,10 +29,41 @@ const DEV_SRC_PATH = './src/dev-assets/';
 
 // Style paths
 const STYLES_SRC_FILES = DEV_SRC_PATH + 'stylesheets/scss/**/!(_*)*.scss';
-const STYLES_DEST_PATH = SRC_PATH + 'stylesheets/css/';
+const STYLES_MIN_DEST_PATH = SRC_PATH + 'stylesheets/css/';
 
 const BOOTSTRAP_SRC_FILES = DEV_SRC_PATH + 'stylesheets/bootstrap/**/!(_*)*.scss';
-const BOOTSTRAP_DEST_PATH = SRC_PATH + 'stylesheets/bootstrap-min/';
+const BOOTSTRAP_MIN_DEST_PATH = SRC_PATH + 'stylesheets/bootstrap-min/';
+
+// Stylesheets deletion pattern
+const STYLES_DEL_PATTERN = [
+  SRC_PATH + 'stylesheets/*/',
+  '!' + SRC_PATH + 'stylesheets/',
+  '!' + SRC_PATH + 'stylesheets/README.md'
+];
+const STYLES_CSS_DEL_PATTERN = [
+  STYLES_MIN_DEST_PATH + '*/',
+  '!' + SRC_PATH + 'stylesheets/README.md'
+];
+const STYLES_BOOTSTRAP_DEL_PATTERN = [
+  BOOTSTRAP_MIN_DEST_PATH + '*/',
+  '!' + SRC_PATH + 'stylesheets/README.md'
+];
+
+/*
+
+Clear assets/stylesheets folder: --------------------------------
+
+*/
+
+async function clear_styles(pattern) {
+  const deletedFilePaths = await del(pattern);
+  console.log('Deleted files:\n', deletedFilePaths.join('\n'));
+  console.log('\n');
+}
+
+gulp.task('clear', () => clear_styles(STYLES_DEL_PATTERN));
+gulp.task('clear-css', () => clear_styles(STYLES_CSS_DEL_PATTERN));
+gulp.task('clear-bootstrap', () => clear_styles(STYLES_BOOTSTRAP_DEL_PATTERN));
 
 /*
 
@@ -46,8 +78,8 @@ async function to_css(source, dest) {
       .pipe(plumber())
       .pipe(sourcemaps.init({loadMaps: true}))
       .pipe(sass().on('error', sass.logError))
-      .pipe(autoprefixer('last 3 version'))
-      .pipe(sourcemaps.write('./', {includeContent: false}))
+      .pipe(autoprefixer())
+      .pipe(sourcemaps.write('./'))
       .pipe(gulp.dest(dest))
       .on('end', resolve);
   });
@@ -64,12 +96,16 @@ async function uglify(dest) {
   return await new Promise((resolve) => {
     gulp.src(dest + '**/!(*.min).css')
       .pipe(plumber())
-      .pipe(cleanCSS({debug: true}, function (details) {
+      .pipe(cleanCSS({debug: true, compatibility: 'ie8'}, function (details) {
         console.log('Original Size : ' + details.name + ': ' + details.stats.originalSize + ' bytes');
         console.log('Minified Size : ' + details.name + ': ' + details.stats.minifiedSize + ' bytes');
       }))
-      .pipe(renameFiles({suffix: '.min'}))
-      .pipe(gulp.dest(dest + 'minified/'))
+      .pipe(rename(function (path) {
+        if (!path.extname.endsWith('.map')) {
+          path.basename += '.min';
+        }
+      }))
+      .pipe(gulp.dest(dest))
       .on('end', resolve);
   });
 }
@@ -81,8 +117,8 @@ Compress normal stylesheets: -------------------------------------
 */
 
 gulp.task('default-stylesheets', async function () {
-  await to_css(STYLES_SRC_FILES, STYLES_DEST_PATH).then(() =>
-    uglify(STYLES_DEST_PATH)
+  await to_css(STYLES_SRC_FILES, STYLES_MIN_DEST_PATH).then(() =>
+    uglify(STYLES_MIN_DEST_PATH)
   );
 });
 
@@ -93,8 +129,8 @@ Compress normal bootstrap: ---------------------------------------
 */
 
 gulp.task('bootstrap-stylesheets', async function () {
-  await to_css(BOOTSTRAP_SRC_FILES, BOOTSTRAP_DEST_PATH).then(() =>
-    uglify(BOOTSTRAP_DEST_PATH)
+  await to_css(BOOTSTRAP_SRC_FILES, BOOTSTRAP_MIN_DEST_PATH).then(() =>
+    uglify(BOOTSTRAP_MIN_DEST_PATH)
   );
 });
 
@@ -104,9 +140,9 @@ Compress Task: ---------------------------------------------------
 
 */
 
-gulp.task('compress', gulp.series('default-stylesheets'));
-gulp.task('bootstrap', gulp.series('bootstrap-stylesheets'));
-gulp.task('combined', gulp.series('bootstrap', 'compress'));
+gulp.task('compress', gulp.series('clear-css', 'default-stylesheets'));
+gulp.task('bootstrap', gulp.series('clear-bootstrap', 'bootstrap-stylesheets'));
+gulp.task('combined', gulp.series('clear', 'bootstrap-stylesheets', 'default-stylesheets'));
 
 /*
 
