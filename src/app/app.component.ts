83d1714608dbd744fefd20648e798f2e4f34ec99
Nolan Bjarne <derjuniorschiemann@gmail.com>
2022-04-03 18:02:22 +0200

    Responsive design improvements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,12 +7,13 @@ export const STYLESHEETS_PATH: string = '../../stylesheets/css/';
   templateUrl: './app.component.html',
   styleUrls: ['../stylesheets/css/app.component.min.css']
 })
-export class AppComponent implements AfterViewInit{
+export class AppComponent implements AfterViewInit {
   @ViewChild('app_root') private app_root!: ElementRef;
 
   ngAfterViewInit() {
     let vh = window.innerHeight * 0.01;
     this.app_root.nativeElement.style.setProperty('--vh', `${vh}px`);
+    // Calculate viewport height --> alternative to css vh unit
     window.addEventListener('resize', () => {
       let vh = window.innerHeight * 0.01;
       this.app_root.nativeElement.style.setProperty('--vh', `${vh}px`);
