7e6167d14c45b0d410143bcfcac58f4e2b0b56e5
Nolan Bjarne <derjuniorschiemann@gmail.com>
2022-04-20 01:02:48 +0200

    Don't use silly :hover css operator on mobile devices --> javascript alternative, which needs a bit more work Frontend cleanup --> made some styling improvements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,49 @@
-import {AfterViewInit, Component, Inject, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, Inject, OnInit, ViewChild} from '@angular/core';
 import {DOCUMENT} from '@angular/common';
 import {WINDOW} from './window-injection.token';
 import {PixSideMenuComponent} from './pix-side-menu/pix-side-menu.component';
-import {faEllipsis} from '@fortawesome/free-solid-svg-icons';
+import {faGear} from '@fortawesome/free-solid-svg-icons';
 import {IconDefinition} from '@fortawesome/free-brands-svg-icons';
 
 export const STYLESHEETS_PATH: string = '../../stylesheets/css/';
+
+const hasMatchMedia = typeof window.matchMedia !== 'undefined';
+/**
+ * Determine if device is touch-capable
+ * true - device is touch-capable
+ * false - device is not touch-capable
+ * null - unable to determine touch capability
+ *
+ * @return {null|boolean}
+ */
+const hasTouch = () => {
+  if (hasMatchMedia) {
+    return window.matchMedia('(hover: none)').matches;
+  }
+  return null;
+};
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['../stylesheets/css/app.component.min.css']
 })
-export class AppComponent implements AfterViewInit {
+export class AppComponent implements OnInit, AfterViewInit {
   @ViewChild(PixSideMenuComponent) private pixSideMenuComponent!: PixSideMenuComponent;
 
-  ellipsis: IconDefinition = faEllipsis;
+  iconSideMenu: IconDefinition = faGear;
+  isTouchTimer: any;
 
   constructor(@Inject(DOCUMENT) private document: Document, @Inject(WINDOW) private readonly window: Window) {
     this.addViewportHeightProperty();
+  }
+
+  ngOnInit() {
+    // Add several event listeners, which help detect touch input
+    this.window.addEventListener('touchstart', this.addTouchClass, false);
+    ['mouseover', 'touchend', 'touchcancel'].forEach(event => {
+      this.window.addEventListener(event, this.removeTouchClass, false);
+    });
   }
 
   ngAfterViewInit() {
@@ -44,5 +69,30 @@ export class AppComponent implements AfterViewInit {
   private addViewportHeightProperty(): void {
     let vh = this.window.innerHeight * 0.01;
     this.document.documentElement.style.setProperty('--vh', `${vh}px`);
+  }
+
+  /**
+   * Add a touch indicator class to the body element
+   *
+   * @private
+   */
+  private addTouchClass(): void {
+    clearTimeout(this.isTouchTimer);
+    let body_element: HTMLElement = this.document.body;
+    if (hasTouch() && body_element.classList.contains('startTouch')) return;
+    body_element.classList.add('startTouch');
+  }
+
+  /**
+   * Remove the touch indicator class from the body element
+   *
+   * @private
+   */
+  private removeTouchClass(): void {
+    this.isTouchTimer = setTimeout(() => {
+      let body_element: HTMLElement = this.document.body;
+      if (hasTouch() && !body_element.classList.contains('startTouch')) return;
+      body_element.classList.remove('startTouch');
+    }, 250);
   }
 }
