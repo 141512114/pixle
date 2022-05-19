602949d2ded4422fb59cd6655f1987f8bfd07fce
Nolan Bjarne <derjuniorschiemann@gmail.com>
2022-05-20 00:26:39 +0200

    Fix very weird bug

--- src/app/pix-grid/pix-grid.component.ts
+++ src/app/pix-grid/pix-grid.component.ts
@@ -41,11 +41,15 @@ export class PixGridComponent implements OnInit, AfterViewInit {
     if (this.grid_image.length <= 0) return;
     this.grid_image_width = this.grid_image[0].length;
     this.grid_image_height = this.grid_image.length;
-    this.window.addEventListener('resize', this.setInitialSizes);
+    this.window.addEventListener('resize', () => {
+      this.setInitialSizes();
+    });
   }
 
   ngAfterViewInit() {
-    this.window.setTimeout(this.setInitialSizes, 10);
+    this.window.setTimeout(() => {
+      this.setInitialSizes();
+    }, 10);
     this.flipWholePixle(false, false);
   }
 
