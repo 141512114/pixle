95d19a3e4dfa8ddd6ae1fa6d9f0e7369f59e3303
Nolan Bjarne <derjuniorschiemann@gmail.com>
2022-05-31 00:28:51 +0200

    Expand emoji database

--- src/app/database/emoji.database.ts
+++ src/app/database/emoji.database.ts
@@ -3,12 +3,125 @@
  * An easy way to store and use these emojis repeatedly throughout different files etc.
  */
 export const WHITE_QUESTIONMARK: number = 0x2754;
+// Simple shapes
 export const REDSQUARE: number = 0x1F7E5;
 export const BLUESQUARE: number = 0x1F7E6;
 export const ORANGESQUARE: number = 0x1F7E7;
 export const YELLOWSQUARE: number = 0x1F7E8;
 export const GREENSQUARE: number = 0x1F7E9;
 export const PURPLESQUARE: number = 0x1F7EA;
+export const REDCIRCLE: number = 0x1F534;
+export const ORANGECIRCLE: number = 0x1F7E0;
+export const YELLOWCIRCLE: number = 0x1F7E1;
+export const GREENCIRCLE: number = 0x1F7E2;
+export const BLUECIRCLE: number = 0x1F535;
+export const PURPLECIRCLE: number = 0x1F7E3;
+// Hearts
+export const REDHEART: number = 0x2764;
+export const ORANGEHEART: number = 0x1F9E1;
+export const GREENHEART: number = 0x1F49A;
+export const BLUEHEART: number = 0x1F499;
+export const PURPLEHEART: number = 0x1F49C;
+// Special
+export const CHERRYBLOSSOM: number = 0x1F4AE;
+export const UPBUTTON: number = 0x1F199;
+export const SQUAREDOK: number = 0x1F197;
+export const BLOODTYPE0: number = 0x1F17E;
+export const BLOODTYPEA: number = 0x1F170;
+export const BLOODTYPEB: number = 0x1F171;
+export const BLOODTYPEAB: number = 0x1F18E;
+export const INFINITYSYMBOL: number = 0x267E;
+export const PEACESYMBOL: number = 0x262E;
+// Clocks
+export const TWELVEOCLOCK: number = 0x1F55B;
+export const TWELVETHIRTY: number = 0x1F567;
+export const ONEOCLOCK: number = 0x1F550;
+export const ONETHIRTY: number = 0x1F55C;
+export const TWOOCLOCK: number = 0x1F551;
+export const TWOTHIRTY: number = 0x1F55D;
+export const THREEOCLOCK: number = 0x1F552;
+export const THREETHIRTY: number = 0x1F55E;
+export const FOUROCLOCK: number = 0x1F553;
+export const FOURTHIRTY: number = 0x1F55F;
+export const FIVEOCLOCK: number = 0x1F554;
+export const FIVETHIRTY: number = 0x1F560;
+export const SIXOCLOCK: number = 0x1F555;
+export const SIXTHIRTY: number = 0x1F561;
+export const SEVENOCLOCK: number = 0x1F556;
+export const SEVENTHIRTY: number = 0x1F562;
+export const EIGHTOCLOCK: number = 0x1F557;
+export const EIGHTTHIRTY: number = 0x1F563;
+export const NINEOCLOCK: number = 0x1F558;
+export const NINETHIRTY: number = 0x1F564;
+export const TENOCLOCK: number = 0x1F559;
+export const TENTHIRTY: number = 0x1F565;
+export const ELEVENOCLOCK: number = 0x1F55A;
+export const ELEVENTHIRTY: number = 0x1F566;
+// Music / Sound
+export const SPEAKERLOW: number = 0x1F508;
+export const SPEAKERMID: number = 0x1F509;
+export const SPEAKERHIGH: number = 0x1F50A;
+export const BELL: number = 0x1F514;
+export const MUSICALNOTE: number = 0x1F3b5;
+// Arrows
+export const ARROWUP: number = 0x2B06;
+export const ARROWUPRIGHT: number = 0x2197;
+export const ARROWRIGHT: number = 0x27A1;
+export const ARROWRIGHTDOWN: number = 0x2198;
+export const ARROWDOWN: number = 0x2B07;
+export const ARROWDOWNLEFT: number = 0x2199;
+export const ARROWLEFT: number = 0x2B05;
+export const ARROWLEFTUP: number = 0x2196;
+// Objects
+export const WRAPPEDGIFT: number = 0x1F381;
+export const CRYSTALBALL: number = 0x1F52E;
+export const FRAMEDPICTURE: number = 0x1F5BC;
+export const GEMSTONE: number = 0x1F48E;
+export const FULLBATTERY: number = 0x1F50B;
+export const LAPTOP: number = 0x1F4BB;
+export const DESKTOPCOMPUTER: number = 0x1F5A5;
+export const PRINTER: number = 0x1F5A8;
+export const OPTICALDISK: number = 0x1F4BF;
+export const DVD: number = 0x1F4C0;
+export const FLOPPYDISK: number = 0x1F4BE;
+export const MINIDISC: number = 0x1F4BD;
+export const CAMERA: number = 0x1F4F7;
+export const TELEVISION: number = 0x1F4FA;
+export const FILMFRAMES: number = 0x1F39E;
+export const VIDEOCASSETTE: number = 0x1F4FC;
+export const REDPAPERLANTERN: number = 0x1F3EE;
+export const REDNOTEBOOK: number = 0x1F4D5;
+export const GREENNOTEBOOK: number = 0x1F4D7;
+export const BLUENOTEBOOK: number = 0x1F4D8;
+export const ORANGENOTEBOOK: number = 0x1F4D9;
+export const COIN: number = 0x1FA99;
+export const PACKAGE: number = 0x1F4E6;
+export const CALENDAR: number = 0x1F4C5;
+export const WINDOW: number = 0x1FA9F;
+export const PILL: number = 0x1F48A;
+export const MAGNET: number = 0x1F9F2;
+export const GEAR: number = 0x2699;
+export const MOYAI: number = 0x1F5FF;
+export const HEADSTONE: number = 0x1FAA6;
+export const SHOPPINGCART: number = 0x1F6D2;
+export const ROLLOFPAPER: number = 0x1F9FB;
+export const TOILET: number = 0x1F6BD;
+export const YARN: number = 0x1F9F6;
+export const SPOOLOFTHREAD: number = 0x1F9F5;
+export const RIBBON: number = 0x1F380;
+export const BRICK: number = 0x1F9F1;
+// Places
+export const MOUNTAIN: number = 0x26F0;
+export const VOLCANO: number = 0x1F30B;
+export const MOUNTFUJI: number = 0x1F5FB;
+export const STADIUM: number = 0x1F3DF;
+export const CLASSICALBUILDING: number = 0x1F3DB;
+export const HOUSE: number = 0x1F3E0;
+export const OFFICE: number = 0x1F3E2;
+export const POSTOFFICE: number = 0x1F3E4;
+export const HOSPITAL: number = 0x1F3E5;
+export const BANK: number = 0x1F3E6;
+export const HOTEL: number = 0x1F3E8;
 
 /**
  * Pixle icon database is stored in this file too
@@ -19,5 +132,110 @@ export const PIXLE_ICONS: number[] = [
   ORANGESQUARE,
   YELLOWSQUARE,
   GREENSQUARE,
-  PURPLESQUARE
+  PURPLESQUARE,
+  REDCIRCLE,
+  ORANGECIRCLE,
+  YELLOWCIRCLE,
+  GREENCIRCLE,
+  BLUECIRCLE,
+  PURPLECIRCLE,
+  REDHEART,
+  ORANGEHEART,
+  GREENHEART,
+  BLUEHEART,
+  PURPLEHEART,
+  CHERRYBLOSSOM,
+  UPBUTTON,
+  SQUAREDOK,
+  BLOODTYPE0,
+  BLOODTYPEA,
+  BLOODTYPEB,
+  BLOODTYPEAB,
+  INFINITYSYMBOL,
+  PEACESYMBOL,
+  TWELVEOCLOCK,
+  TWELVETHIRTY,
+  ONEOCLOCK,
+  ONETHIRTY,
+  TWOOCLOCK,
+  TWOTHIRTY,
+  THREEOCLOCK,
+  THREETHIRTY,
+  FOUROCLOCK,
+  FOURTHIRTY,
+  FIVEOCLOCK,
+  FIVETHIRTY,
+  SIXOCLOCK,
+  SIXTHIRTY,
+  SEVENOCLOCK,
+  SEVENTHIRTY,
+  EIGHTOCLOCK,
+  EIGHTTHIRTY,
+  NINEOCLOCK,
+  NINETHIRTY,
+  TENOCLOCK,
+  TENTHIRTY,
+  ELEVENOCLOCK,
+  ELEVENTHIRTY,
+  SPEAKERLOW,
+  SPEAKERMID,
+  SPEAKERHIGH,
+  BELL,
+  MUSICALNOTE,
+  ARROWUP,
+  ARROWUPRIGHT,
+  ARROWRIGHT,
+  ARROWRIGHTDOWN,
+  ARROWDOWN,
+  ARROWDOWNLEFT,
+  ARROWLEFT,
+  ARROWLEFTUP,
+  WRAPPEDGIFT,
+  CRYSTALBALL,
+  FRAMEDPICTURE,
+  GEMSTONE,
+  FULLBATTERY,
+  LAPTOP,
+  DESKTOPCOMPUTER,
+  PRINTER,
+  OPTICALDISK,
+  DVD,
+  FLOPPYDISK,
+  MINIDISC,
+  CAMERA,
+  TELEVISION,
+  FILMFRAMES,
+  VIDEOCASSETTE,
+  REDPAPERLANTERN,
+  REDNOTEBOOK,
+  GREENNOTEBOOK,
+  BLUENOTEBOOK,
+  ORANGENOTEBOOK,
+  COIN,
+  PACKAGE,
+  CALENDAR,
+  WINDOW,
+  PILL,
+  MAGNET,
+  GEAR,
+  MOYAI,
+  HEADSTONE,
+  SHOPPINGCART,
+  ROLLOFPAPER,
+  TOILET,
+  YARN,
+  SPOOLOFTHREAD,
+  RIBBON,
+  BRICK,
+  MOUNTAIN,
+  VOLCANO,
+  MOUNTFUJI,
+  STADIUM,
+  CLASSICALBUILDING,
+  HOUSE,
+  OFFICE,
+  POSTOFFICE,
+  HOSPITAL,
+  BANK,
+  HOTEL
 ];
