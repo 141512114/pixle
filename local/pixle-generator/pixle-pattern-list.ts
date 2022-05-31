d7af758590012d09d1a10d067c57b813190b0e35
Nolan Bjarne <derjuniorschiemann@gmail.com>
2022-06-01 00:03:33 +0200

    Expand available patterns

--- local/pixle-generator/pixle-pattern-list.ts
+++ local/pixle-generator/pixle-pattern-list.ts
@@ -4,8 +4,19 @@ import * as patterns from './pixle-patterns.database';
  * The master array list which contains all possible pixle patterns
  */
 export const MASTER_PATTERNS: any[] = [
+  patterns.TTOWERPATTERN,
+  patterns.TWIRLPATTERN,
+  patterns.THREESTRIPESH2,
   patterns.HPATTERN,
+  patterns.CHECKERDPATTERN,
   patterns.CROSSPATTERN,
   patterns.TOWERPATTERN,
-  patterns.RINGPATTERN
+  patterns.CASCADINGPATTERN,
+  patterns.SMALLCIRCLEPATTERN,
+  patterns.THREESTRIPESH,
+  patterns.RINGPATTERN,
+  patterns.CONFUSEDPATTERN,
+  patterns.LIGHTINGPATTERN,
+  patterns.HOUSEPATTERN,
+  patterns.CAROUSELPATTERN
 ];
--- local/pixle-generator/pixle-patterns.database.ts
+++ local/pixle-generator/pixle-patterns.database.ts
@@ -7,10 +7,38 @@ It is as easy as that.
 
 */
 
+export const THREESTRIPESH: number[][] = [
+  [1, 1, 1],
+  [0, 0, 0],
+  [1, 1, 1],
+  [0, 0, 0],
+  [1, 1, 1]
+];
+
+export const THREESTRIPESH2: number[][] = [
+  [1, 1, 1],
+  [0, 0, 0],
+  [2, 2, 2],
+  [0, 0, 0],
+  [1, 1, 1]
+];
+
 export const HPATTERN: number[][] = [
   [0, 1, 0],
   [0, 0, 0],
   [0, 1, 0]
+];
+
+export const SMALLCIRCLEPATTERN: number[][] = [
+  [1, 1, 1],
+  [1, 0, 1],
+  [1, 1, 1]
+];
+
+export const TWIRLPATTERN: number[][] = [
+  [1, 2, 1],
+  [2, 3, 2],
+  [1, 2, 1]
 ];
 
 export const CROSSPATTERN: number[][] = [
@@ -20,11 +48,35 @@ export const CROSSPATTERN: number[][] = [
 ];
 
 export const TOWERPATTERN: number[][] = [
-  [1, 0, 4],
+  [1, 2, 1],
+  [1, 0, 1],
+  [1, 0, 1],
+  [1, 2, 1],
+  [1, 2, 1]
+];
+
+export const TTOWERPATTERN: number[][] = [
+  [1, 1, 1],
+  [3, 1, 3],
+  [2, 1, 2],
+  [1, 1, 1],
+  [0, 0, 0]
+];
+
+export const CASCADINGPATTERN: number[][] = [
+  [4, 4, 4],
+  [3, 3, 3],
+  [2, 2, 2],
+  [1, 1, 1],
+  [0, 0, 0]
+];
+
+export const CONFUSEDPATTERN: number[][] = [
+  [0, 0, 4],
   [3, 1, 2],
   [3, 1, 3],
   [2, 1, 3],
-  [4, 0, 1]
+  [4, 0, 0]
 ];
 
 export const RINGPATTERN: number[][] = [
@@ -32,4 +84,32 @@ export const RINGPATTERN: number[][] = [
   [1, 0, 2, 1],
   [1, 2, 0, 1],
   [1, 1, 1, 1]
+];
+
+export const CAROUSELPATTERN: number[][] = [
+  [0, 1, 0, 0],
+  [0, 1, 2, 2],
+  [2, 2, 1, 0],
+  [0, 0, 1, 0]
+];
+
+export const HOUSEPATTERN: number[][] = [
+  [0, 0, 0, 0],
+  [0, 1, 0, 0],
+  [1, 3, 1, 0],
+  [1, 2, 1, 4]
+];
+
+export const CHECKERDPATTERN: number[][] = [
+  [0, 1, 0, 1],
+  [2, 0, 2, 0],
+  [0, 1, 0, 1],
+  [2, 0, 2, 0]
+];
+
+export const LIGHTINGPATTERN: number[][] = [
+  [2, 1, 0, 1],
+  [1, 1, 1, 0],
+  [0, 1, 2, 0],
+  [2, 0, 1, 0]
 ];
