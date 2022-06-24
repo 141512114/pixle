0e1f6f80cd569514f4af82b2264e153236c55647
Nolan Bjarne <derjuniorschiemann@gmail.com>
2022-06-25 01:38:30 +0200

    Add cookie alert to the landing page too

--- projects/pixle-landing/src/app/app.component.ts
+++ projects/pixle-landing/src/app/app.component.ts
@@ -1,11 +1,21 @@
-import {Component, ElementRef, Inject, OnInit, ViewChild, ViewEncapsulation} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, Inject, OnInit, ViewChild, ViewEncapsulation} from '@angular/core';
 import {HelperFunctionsService} from '../../../../local/typescript/abstract/services/helper-functions.service';
 import {DOCUMENT} from '@angular/common';
 import {IconDefinition} from '@fortawesome/free-brands-svg-icons';
 import {faGear, faXmark} from '@fortawesome/free-solid-svg-icons';
 import {SideMenuComponent} from '../../../../local/typescript/side-menu/side-menu.component';
+import {IPopUp} from '../../../../local/typescript/interface/popup-message.interface';
+import {PopupMessageComponent} from '../../../../local/typescript/popup-message/popup-message.component';
 
 export const STYLESHEETS_PATH: string = '../../stylesheets/css/';
+
+// Cookie notification
+const COOKIE_NOTIF_MSG: IPopUp = {
+  headline: 'Cookie alert!',
+  subline: '',
+  message_body: 'We are required to inform you about the usage of cookies on our website. These are required' +
+    ' and used in order to save applied settings. Closing this notification means you agree with the usage of cookies.'
+};
 
 @Component({
   encapsulation: ViewEncapsulation.None,
@@ -13,10 +23,12 @@ export const STYLESHEETS_PATH: string = '../../stylesheets/css/';
   templateUrl: './app.component.html',
   styleUrls: ['../stylesheets/css/app.component.min.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, AfterViewInit {
+  cookie_consent: boolean = false;
   iconOpenSideMenu: IconDefinition = faGear;
   iconCloseSideMenu: IconDefinition = faXmark;
   @ViewChild(SideMenuComponent) private sideMenuComponent!: SideMenuComponent;
+  @ViewChild('cookie_alert') private cookie_alert!: PopupMessageComponent;
   @ViewChild('toggle_side_menu_btn') private toggle_side_menu_btn!: ElementRef;
 
   constructor(@Inject(DOCUMENT) private document: Document) {
@@ -24,17 +36,43 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     // Get the stored theme data, if available, and "restore" the previous settings
-    let previous_theme: string | null = null;
-    if (HelperFunctionsService.isLocalStorageAvailable()) {
-      previous_theme = localStorage.getItem('last_theme');
-    } else {
-      if (HelperFunctionsService.isSessionStorageAvailable()) {
-        previous_theme = sessionStorage.getItem('last_theme');
-      }
-    }
+    let previous_theme: string | null = HelperFunctionsService.getCookie('last_theme');
     if (previous_theme != null) {
       this.document.body.dataset['theme'] = previous_theme;
     }
+    // Check if the consent to the cookie usage has already been given
+    if (HelperFunctionsService.getCookie('cookie_consent') === '1') {
+      this.cookie_consent = true;
+    }
+  }
+
+  ngAfterViewInit() {
+    if (!this.cookie_consent) {
+      this.sendMatchMessage(COOKIE_NOTIF_MSG);
+    }
+  }
+
+  /**
+   * Receive the emitted event which tells the game component if a popup has been closed
+   *
+   * @param paket
+   */
+  public receivePopupHasBeenClosed(paket: boolean = false): void {
+    this.cookie_consent = paket;
+    if (this.cookie_consent) {
+      HelperFunctionsService.createCookie('cookie_consent', '1');
+    }
+  }
+
+  /**
+   * Receive the current status of the ongoing match and evaluate the status number
+   *
+   * @param msg_object
+   */
+  public sendMatchMessage(msg_object: IPopUp): void {
+    let popup_msg: PopupMessageComponent = this.cookie_alert;
+    popup_msg.writeNewMessage(msg_object);
+    popup_msg.addClassToHTMLElement(popup_msg.msg_container.nativeElement);
   }
 
   /**
